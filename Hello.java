9a5ab1c353425966f9075bba7ee2437843603ae2
DivyaShetty2412 <68385481+DivyaShetty2412@users.noreply.github.com>
2021-04-16 15:59:44 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello{
 public static void main(String[] args)
 {
-for(int i=1;i&lt;=15;i++)
+for(int i=1;i<=15;i++)
 {
-System.out.println(&quot;hi sjcem&quot;+i);
+System.out.println("hi sjcem"+i);
 }
 }
 }
